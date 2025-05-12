731968b245cc6488eb9b0e640f2593d0858da0a6
Nardellar <simonenardella2003@gmail.com>
2025-05-12 15:03:33 +0200

    "lll"

--- transformer_model_temperatura.py
+++ transformer_model_temperatura.py
@@ -1,4 +1,3 @@
-import tensorflow as tf
 import tensorflow_hub as hub
 from tensorflow.keras import layers, models
 from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
