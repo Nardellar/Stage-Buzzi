9b98dd3a9019306249dab5b08c196ce21ad9f23c
Nardellar <simonenardella2003@gmail.com>
2025-06-11 11:53:46 +0200

    o

--- Classificazione/ViT/VIT_di_rpova_temperatura.py
+++ Classificazione/ViT/VIT_di_rpova_temperatura.py
@@ -1,4 +1,18 @@
-from huggingface_hub import notebook_login
+from transformers import AutoImageProcessor, TFViTForImageClassification
+import tensorflow as tf
+from datasets import load_dataset
 
-notebook_login()
-#SONO ARRIVATO A "Set Up" della guida huggingface, in sezione Quickstart
+if __name__ == "__main__":
+
+    dataset = load_dataset("huggingface/cats-image", trust_remote_code=True)
+    image = dataset["test"]["image"][0]
+
+    image_processor = AutoImageProcessor.from_pretrained("google/vit-base-patch16-224")
+    model = TFViTForImageClassification.from_pretrained("google/vit-base-patch16-224")
+
+    inputs = image_processor(image, return_tensors="tf")
+    logits = model(**inputs).logits
+
+    # model predicts one of the 1000 ImageNet classes
+    predicted_label = int(tf.math.argmax(logits, axis=-1))
+    print(model.config.id2label[predicted_label])
